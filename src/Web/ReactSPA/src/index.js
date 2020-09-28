8aa1f1d305807768356168f48446c445827054f6
Jairon Alves Lima <jairon.alves.lima@gmail.com>
2020-09-28 20:01:23 -0300

    Need more test

--- src/Web/ReactSPA/src/index.js
+++ src/Web/ReactSPA/src/index.js
@@ -2,18 +2,14 @@ import React from 'react'
 import { render as ReactDOMRender } from 'react-dom'
 import App from './App'
 
-const PREVIEW =
-  typeof process.env.DEPLOY_ENV === 'string' && process.env.DEPLOY_ENV !== 'production'
-
-if (PREVIEW) {
-  const enviroment = {
+const enviroment = {
     NODE_ENV: process.env.NODE_ENV,
     DEPLOY_ENV: process.env.DEPLOY_ENV,
     API_BASE_URL: process.env.API_BASE_URL,
   }
   console.log('ENVIROMENT')
   console.log(enviroment)
-}
+
 
 ReactDOMRender(<App />, document.querySelector('[data-js="app"]'))
 
